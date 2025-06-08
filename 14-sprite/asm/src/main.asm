6aa2b52a09f6864dc94a7bfc33fae32979400f3d
Andy McCall <mailme@andymccall.co.uk>
2025-06-08 11:32:48 +0100

    chore(): enabled sprite layer without mouse

--- 14-sprite/asm/src/main.asm
+++ 14-sprite/asm/src/main.asm
@@ -60,6 +60,9 @@
    .endscope
 .endmacro
 
+; https://github.com/X16Community/x16-docs/blob/master/X16%20Reference%20-%2009%20-%20VERA%20Programmer's%20Reference.md#9f29-9f2c
+DC_VIDEO          = $9F29
+
 ; Zero Page
 ZP_PTR            = $30
 DISPLAY_SCALE     = 64
@@ -85,37 +88,52 @@ VERA_ctrl         = $9F25
 MOUSE_CONFIG      = $FF68
 
 sprite:
-.byte $00,$00,$0B,$BB,$BB,$B0,$00,$00
-.byte $00,$00,$B6,$66,$66,$6B,$00,$00
-.byte $00,$0B,$66,$66,$66,$66,$B0,$00
-.byte $00,$0B,$66,$66,$66,$66,$B0,$00
-.byte $00,$B6,$6B,$BB,$BB,$B6,$6B,$00
-.byte $00,$B6,$B7,$77,$77,$7B,$6B,$00
-.byte $00,$B6,$77,$B7,$7B,$77,$6B,$00
-.byte $00,$B6,$77,$77,$77,$77,$6B,$00
-.byte $00,$B6,$67,$77,$77,$76,$6B,$00
-.byte $00,$0B,$66,$66,$66,$66,$B0,$00
-.byte $00,$00,$B6,$BB,$BB,$6B,$00,$00
-.byte $00,$B6,$66,$6B,$B6,$66,$6B,$00
-.byte $00,$0B,$66,$66,$66,$66,$B0,$00
-.byte $00,$B6,$66,$6B,$B6,$66,$6B,$00
-.byte $00,$B6,$66,$6B,$B6,$66,$6B,$00
-.byte $00,$0B,$66,$BB,$BB,$66,$B0,$00
+; Row 0 - Empty/Hair
+.byte $00,$00,$00,$00,$00,$00,$00,$00
+; Row 1 - Hair/Head Top
+.byte $00,$08,$88,$88,$88,$80,$00,$00
+; Row 2 - Head/Forehead
+.byte $08,$11,$22,$22,$22,$18,$00,$00
+; Row 3 - Eye/Rotten Flesh
+.byte $81,$24,$44,$22,$22,$21,$80,$00 ; Left eye socket, decay
+; Row 4 - Eye/Nose
+.byte $12,$22,$22,$22,$22,$22,$10,$00
+; Row 5 - Jaw/Mouth
+.byte $21,$22,$11,$11,$12,$21,$20,$00 ; Sunken mouth
+; Row 6 - Neck/Shoulder
+.byte $00,$12,$22,$22,$22,$21,$00,$00
+; Row 7 - Shoulder/Torso (slight hunch)
+.byte $00,$67,$77,$77,$77,$77,$60,$00
+; Row 8 - Torso/Arm (tattered)
+.byte $06,$77,$77,$77,$77,$77,$76,$00
+; Row 9 - Torso/Arm
+.byte $77,$77,$77,$77,$77,$77,$77,$70
+; Row 10 - Tattered Clothing/Missing Arm (more decayed)
+.byte $07,$77,$77,$00,$00,$77,$70,$00 ; Hole/decay on left side
+; Row 11 - Waist/Legs
+.byte $00,$77,$77,$77,$77,$77,$70,$00
+; Row 12 - Legs (more asymmetric)
+.byte $00,$67,$76,$00,$67,$76,$60,$00 ; One leg more decayed/missing lower part
+; Row 13 - Legs
+.byte $00,$67,$76,$00,$67,$76,$60,$00
+; Row 14 - Feet/Stumps
+.byte $00,$00,$66,$00,$00,$66,$00,$00
+; Row 15
+.byte $00,$00,$00,$00,$00,$00,$00,$00
 end_sprite:
 SPRITE_SIZE = end_sprite-sprite
-
 
 frame: .word 0
 
 start:
 
-   ; initialize
-   lda #<(VRAM_sprite_frames >> 5)
+; Initialize
+   lda #<(VRAM_sprite_frames >> 1)
    sta frame
    
-   ;------------------
-   ; init sprite
-   ;------------------
+;------------------
+; Init sprite
+;------------------
    stz VERA_ctrl
    VERA_SET_ADDR sprite2_sprattr, 1
    lda frame
@@ -136,9 +154,31 @@ start:
    lda #(SPRITE_16H | SPRITE_16W)
    sta VERA_data0
 
-   jsr MOUSE_CONFIG
+;----------------------------------
+; Setup the display to show sprites
+;----------------------------------
+   ; Enable sprites by pushing config into DC_VIDEO
+   ; Example: Enable Sprites, Layer 0, Layer 1, and VGA output
+   ; %01110001 (binary) = $71 (hex)
+   ; Bit 7: 1 (Sprites enabled)
+   ; Bit 6: 1 (Layer 1 enabled)
+   ; Bit 5: 1 (Layer 0 enabled)
+   ; Bit 4: 0 (assuming default tile base)
+   ; Bit 3: 0 (scanline interrupt disabled)
+   ; Bit 2: 0 (line doubler disabled)
+   ; Bit 1: 0 (VGA mode bit 1)
+   ; Bit 0: 1 (VGA mode bit 0)
+   ; Note: For VGA, bits 1 and 0 of $9F29 usually form 01b for 640x480, or 10b for 320x240 (with line doubling).
+   ; The most common VGA setup usually results in a value of $71 if you want 640x480 with both layers.
 
+; Load the current config
+   lda DC_VIDEO
+; Or the existing config with the config I want, based on the bitmask above
+   ora #%01110000
+; Write it back to the DC_VIDEO address
+   sta DC_VIDEO
     
+; Finally copy the sprite to the video ram
    RAM2VRAM sprite, VRAM_sprite_frames, SPRITE_SIZE
 
 
